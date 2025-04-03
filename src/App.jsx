0fecdd3e0ae4d5578392508a0e56441ce05da9d2
lucassilvahomem <lucas_homem@estudante.sesisenai.org.br>
2025-04-02 21:15:24 -0300

    Lançado MVP do projeto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { IoLogoGithub } from "react-icons/io";
+
+function App() {
+
+  return (
+    <div className='container-app'>
+      <h1>Links lucasH</h1>
+
+      <a href="https://github.com/lucassilvahomem" className='links' target='_blank'> 
+      <IoLogoGithub />
+        github 
+      </a>
+    </div>
+  )
+}
+
+export default App
